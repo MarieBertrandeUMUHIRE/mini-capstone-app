03f23441c31ff98e4b7a0a77f687cd23a847e002
MarieBertrandeUMUHIRE <mariebertrande.umuhire@gmail.com>
2017-01-23 16:30:04 +0200

    I have edited the mini-capstone-app

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,23 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+   @product = Product.find_by(id: params[:id])
+ end
+
+ def new
+ end
+
+ def create
+  name = params[:name]
+  price = params[:price]
+  image = params[:image]
+  description = params[:description]
+  product = Product.new({name: name, price: price, image: image, description: description })
+  product.save
+end
+
+end
