a8a0af506bbd137e5e1081f40b66bff39411b4aa
MarieBertrandeUMUHIRE <mariebertrande.umuhire@gmail.com>
2017-01-24 14:35:05 +0200

    I have added the delete option

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,19 +5,38 @@ class ProductsController < ApplicationController
   end
 
   def show
-   @product = Product.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
  end
 
- def new
- end
+  def new
+
+  end
 
  def create
-  name = params[:name]
-  price = params[:price]
-  image = params[:image]
-  description = params[:description]
-  product = Product.new({name: name, price: price, image: image, description: description })
-  product.save
-end
+    name = params[:name]
+    price = params[:price]
+    image = params[:image]
+    description = params[:description]
+    product = Product.new({name: name, price: price, image: image, description: description })
+    product.save
+  end
 
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image = params[:image]
+    @product.description = params[:description]
+    @product.save
+  end
+
+  def destroy
+     @product = Product.find_by(id: params[:id])
+     @product.destroy
+  end
 end
